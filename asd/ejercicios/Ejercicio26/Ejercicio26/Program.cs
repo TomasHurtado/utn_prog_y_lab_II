1f09f829aa913ea24392342b151ac14fb01fa689
Tomas Hurtado <tomas221298@gmail.com>
2019-09-10 22:10:55 -0300

    gg

---
+++ asd/ejercicios/Ejercicio26/Ejercicio26/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] vector;
+            vector = new int[20];
+            
+            for (int i=0;i< vector.Length;i++)
+            {
+                vector[i] = rnd.Next(-9999,9999);
+            }
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write(vector[i]+"\n");
+            }
+
+            //prop y 2 veces tab  public int MyProperty { get; set; }
+
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i]>0)
+                {
+                    Console.Write(vector[i] + "positivos: "+vector[i]);
+                }
+                else
+                {
+                    Console.Write(vector[i] + "negativos: " + vector[i]);
+                }
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
